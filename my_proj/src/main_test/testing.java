9cbaa2e4e61673453d4508938cfddeb262a4142f
keerthan156 <108721283+keerthan156@users.noreply.github.com>
2022-08-11 15:32:11 +0530

    Update testing.java

--- my_proj/src/main_test/testing.java
+++ my_proj/src/main_test/testing.java
@@ -1,1 +1,46 @@
-.
+package com.scmgalaxy.mavensample;
+
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+
+/**
+* Unit test for simple App.
+*/
+public class test1
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public test1( String testName )
+    {
+        super( testName );
+    }
+
+
+
+   /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( test1.class );
+    }
+
+
+
+   /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
